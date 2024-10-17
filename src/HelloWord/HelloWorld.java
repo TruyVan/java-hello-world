3516ee14a5d3990bcf9dfb8ce7319bb1bc07ccce
TruyVan <Truyvanlangkhach2512@gmail.com>
2024-10-17 11:11:54 +0700

    init Hello World project

---
+++ src/HelloWord/HelloWorld.java
@@ -0,0 +1,9 @@
+package HelloWord;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World, with Po");
+		System.out.println("Hello World, with Po");
+		System.out.println("Hello World, with Po");
+	}
+}
